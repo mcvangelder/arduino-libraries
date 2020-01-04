85d122280a9eb0018f985841a6e10099fd69ba87
Matthew Van Gelder <vangelder@gmail.com>
2020-01-03 18:19:27 -0800

    Added ability to override default pin configuration

--- NFC-MiFareReader/nfc-mifarereader-i2c.h
+++ NFC-MiFareReader/nfc-mifarereader-i2c.h
@@ -7,7 +7,7 @@
 class NFCMiFareReaderI2C : public NFCMiFareReader
 {
 public:
-    NFCMiFareReaderI2C();
+    NFCMiFareReaderI2C(uint8_t pn532IRQ = PN532_IRQ, uint8_t pn532ResetPin = PN532_RESET);
     void initialize();
     uint8_t read(ReadStatus &status);
     bool activateCardReader();
